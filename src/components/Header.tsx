afe2c57863e85eb56b2578b871fb035f84b63fc1
daniv <danivego2015@gmail.com>
2025-06-10 16:48:15 -0500

    UI improvements 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Header.tsx 	modified:   src/layouts/Layout.astro 	modified:   src/pages/index.astro 	modified:   src/styles/global.css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,7 @@
+import { Briefcase, Code, Home, Mail } from "lucide-react";
 import { useEffect, useState } from "react";
+
+// TODO: Work on mobile version
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -13,19 +16,40 @@ const Header = () => {
 
   return (
     <header
-      className={`w-full md:w-3xl lg:w-4xl h-16 fixed top-0 z-50 px-2 mt-2 rounded-full ${
-        isScrolled &&
-        "bg-cteal/20 backdrop-blur-sm border border-pblue shadow-md"
-      } transition-colors duration-300`}
+      className={`w-full md:w-3xl lg:w-4xl h-12 fixed top-0 z-50 px-2 mt-2 rounded-full ${
+        isScrolled && "bg-jet/10 backdrop-blur-sm border border-jet shadow-md"
+      } transition-all duration-300 contain-content`}
     >
       <div className="flex justify-between items-center h-full px-4">
-        <a href="/" className="text-black/80 text-2xl font-bold">
+        <a href="/#" className="text-2xl font-bold">
           Daniel Velasco
         </a>
         <div className="flex gap-4">
-          <a href="/">Home</a>
-          <a href="/#experience">Experience</a>
-          <a href="/#contact">Contact</a>
+          <a href="/#" className="flex items-center gap-2 hover:underline">
+            <Home />
+            Home
+          </a>
+          <a
+            href="/#experience"
+            className="flex items-center gap-2 hover:underline"
+          >
+            <Briefcase />
+            Experience
+          </a>
+          <a
+            href="/#projects"
+            className="flex items-center gap-2 hover:underline"
+          >
+            <Code />
+            Projects
+          </a>
+          <a
+            href="/#contact"
+            className="flex items-center gap-2 hover:underline"
+          >
+            <Mail />
+            Contact
+          </a>
         </div>
       </div>
     </header>
